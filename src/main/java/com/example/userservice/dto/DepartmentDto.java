aefc096cad5f1c93232e436de183c414f4c13129
ganemchedli <ganemchedli@gmail.com>
2023-05-07 03:04:26 +0100

    First commit

---
+++ src/main/java/com/example/userservice/dto/DepartmentDto.java
@@ -0,0 +1,23 @@
+package com.example.userservice.dto;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "departments")
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+public class DepartmentDto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String departmentName;
+    private String departmentAddress;
+    private String departmentCode;
+}
